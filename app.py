e630e35c9285865555d944bd6132bc6a08359096
trey-rosius <treyrosius@gmail.com>
2022-08-10 14:23:27 +0100

    first commit

--- app.py
+++ app.py
@@ -5,24 +5,23 @@ import aws_cdk as cdk
 
 from cdk_momo.cdk_momo_stack import CdkMomoStack
 
-
 app = cdk.App()
 CdkMomoStack(app, "CdkMomoStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+             # If you don't specify 'env', this stack will be environment-agnostic.
+             # Account/Region-dependent features and context lookups will not work,
+             # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
+             # Uncomment the next line to specialize this stack for the AWS Account
+             # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+             # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
+             # Uncomment the next line if you know exactly what Account and Region you
+             # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+             env=cdk.Environment(account="132260253285", region='us-east-2'),
 
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+             # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+             )
 
 app.synth()
---
+++ cdk_momo/lambda/start_step_function.py
@@ -0,0 +1,18 @@
+import json
+import boto3
+
+step_function_client = boto3.client("stepfunctions")
+
+
+def handler(event, context):
+    print("Lambda function invoked")
+    print(json.dumps(event))
+    print(json.dumps(event["arguments"]['input']))
+    response = step_function_client.start_execution(
+        stateMachineArn=event["arguments"]['input']['version'],
+        name=event["arguments"]['input']['id'],
+        input= "{\"details\":{\"accountId\":\"1234567\",\"bookedStatus\":\"Booked\"}}",
+
+    )
+
+    return {"id": event["arguments"]['input']['id'], "version": event["arguments"]['input']['version']}
